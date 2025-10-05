d4a3e4b9f95bfd41f8e0b58771c1ca164e83b406
syedasheesh <syedabupower@gmail.com>
2025-10-06 05:29:33 +0530

    Update main.kt

--- main.kt
+++ main.kt
@@ -6,3 +6,16 @@ fun main() {
     val asheesh = true
     print("$syed $abu $asheesh")
 }
+/*
+  Int in Kotlin = 32-bit signed integer
+  - Uses 32 bits (1 for sign, 31 for value)
+  - Range: -2,147,483,648 to 2,147,483,647
+  - Int.MAX_VALUE = 2147483647 (2^31 - 1)
+  - Int.MIN_VALUE = -2147483648 (-2^31)
+*/
+
+fun main() {
+    println(Int.MAX_VALUE) // 2147483647
+    println(Int.MIN_VALUE) // -2147483648
+}
+
